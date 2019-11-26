1384f59913342f1f1aa80c2cca8418af65d0d62c
acsarda <sardaugusto@gmail.com>
2019-11-26 18:16:35 -0300

    actualizoooooooo

--- TP1AllanVariance/simulador.m
+++ TP1AllanVariance/simulador.m
@@ -2,9 +2,9 @@ clear;
 close all;
 
 % desviosRuidos=[2e-4;8e-3;.1;1;5];
-desviosRuidos=[0;0.03;18;0;0];%MEMS
+% desviosRuidos=[0;0.03;18;0;0];%MEMS
 desviosRuidos=[0;.015;0;.1;0];%RLG
-Ts=.004;
+Ts=.1;
 Fs=1/Ts;
 TSim=5;
 
